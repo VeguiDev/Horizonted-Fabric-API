47ccb0989c51a3868a5972599a878621b151d1d8
VeguiDev <veguigameplays@gmail.com>
2026-04-21 12:32:48 -0300

    fix: correct entrypoint name and update fabricapi version

--- testmod/src/main/java/dev/vegui/hfa/testmod/TestMod.java
+++ testmod/src/main/java/dev/vegui/hfa/testmod/TestMod.java
@@ -1,15 +1,11 @@
 package dev.vegui.hfa.testmod;
 
-import io.canvasmc.horizon.HorizonLoader;
-import io.canvasmc.horizon.logger.Logger;
 import io.canvasmc.horizon.service.entrypoint.DedicatedServerInitializer;
 
 public class TestMod implements DedicatedServerInitializer {
 
-    public static Logger LOGGER = HorizonLoader.LOGGER;
-
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello from TestMod!");
+        System.out.println("[HFA Test Mod] server_postbootstrap invoked");
     }
 }
