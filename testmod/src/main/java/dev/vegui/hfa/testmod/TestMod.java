e28e54687a428097f2f62b93673ed819d7671075
VeguiDev <veguigameplays@gmail.com>
2026-05-11 01:59:25 -0300

    Add bundled module dependencies and server lifecycle event logging

--- testmod/src/main/java/dev/vegui/hfa/testmod/TestMod.java
+++ testmod/src/main/java/dev/vegui/hfa/testmod/TestMod.java
@@ -1,11 +1,18 @@
 package dev.vegui.hfa.testmod;
 
 import io.canvasmc.horizon.service.entrypoint.ServerPostBootstrapEntrypoint;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class TestMod implements ServerPostBootstrapEntrypoint {
 
     @Override
     public void onInitialize() {
         System.out.println("[HFA Test Mod] server_postbootstrap invoked");
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            System.out.println("[HFA Test Mod] Server started");
+        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            System.out.println("[HFA Test Mod] Server stopping bye");
+        });
     }
 }
